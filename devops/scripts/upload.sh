eb7e3a853f7ae97f6513f25a831a33f6bee4afd5
bhairavaswamyvijjapu <82381651+bhairavaswamyvijjapu@users.noreply.github.com>
2021-04-13 00:10:45 +0530

    Update upload.sh

--- devops/scripts/upload.sh
+++ devops/scripts/upload.sh
@@ -8,7 +8,7 @@ echo "S3 Bucket Name: ${S3_BUCKET}"
 echo "Uploading Artifacts"
 aws s3 cp "${CI_PROJECT_DIR}/artifacts/" "s3://${S3_BUCKET}/" --recursive
 
-cp -f "${CI_PROJECT_DIR}/devops/cloudformation/lambdas-version.yaml" "${CI_PROJECT_DIR}/devops/cloudformation/lambdas.yaml"
+cp -f "${CI_PROJECT_DIR}/devops/cloudformation/lambdas-version.yaml" "${CI_PROJECT_DIR}/devops/cloudformation/lambdas-version.yaml"
 
 
 # Update Lambda S3 Version in CF Template
@@ -20,7 +20,7 @@ for lambda in ${lambdas_list[@]};
 do
     VERSION_ID=$(aws s3api put-object-tagging --bucket "${S3_BUCKET}" --key "lambdas/${lambda}.zip" --tagging 'TagSet=[{Key=lambda,Value=getVersion}]' --output text)
     echo "$lambda : $VERSION_ID"
-    sed -i "s/<${lambda}-s3-version>/${VERSION_ID}/g" "${CI_PROJECT_DIR}/devops/cloudformation/lambdas.yaml"
+    sed -i "s/<${lambda}-s3-version>/${VERSION_ID}/g" "${CI_PROJECT_DIR}/devops/cloudformation/lambdas-version.yaml"
 done;
 
 # echo "Uploading cloudformation files"
